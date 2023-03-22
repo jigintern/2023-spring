e1ed2790b67618ae697b89d53d8f7db7439ac241
Kento Suzuki <suzuken2960@gmail.com>
2023-03-22 16:43:07 +0900

    test.jsの削除

--- fetchChat.js
+++ fetchChat.js
@@ -5,37 +5,40 @@ import { DateTime } from "https://js.sabae.cc/DateTime.js";
 const KEY = Deno.env.get("OPENAI_API_KEY");
 
 const fetchCompletions = async (req) => {
-    const url = "https://api.openai.com/v1/chat/completions";
-    const opt = req ? {
+  const url = "https://api.openai.com/v1/chat/completions";
+  const opt = req
+    ? {
         method: "POST",
         mode: "cors",
         cache: "no-cache",
         headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + KEY,
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + KEY,
         },
         body: JSON.stringify(req),
-    } : null;
-    const res = await (await fetch(url, opt)).json();
-    return res;
+      }
+    : null;
+  const res = await (await fetch(url, opt)).json();
+  return res;
 };
 
 await Deno.mkdir("log", { recursive: true });
 
 export const fetchChat = async (prompt) => {
-    console.log(KEY);
-    const req = {
-        model: "gpt-3.5-turbo", // gpt-3.5-turbo and gpt-3.5-turbo-0301 are supported.
-        messages: [
-            { "role": "user", "content": prompt },
-        ],
-    };
-    //console.log(req);
-    const res = await fetchCompletions(req);
-    //console.log(res);
-    const answer = res.choices[0].message.content;
-    const dt = new DateTime();
-    const data = { dt, prompt, answer };
-    await Deno.writeTextFile("log/" + dt.day.toString() + ".ndjson", JSON.stringify(data) + "\n");
-    return answer;
+  console.log(KEY);
+  const req = {
+    model: "gpt-3.5-turbo", // gpt-3.5-turbo and gpt-3.5-turbo-0301 are supported.
+    messages: [{ role: "user", content: prompt }],
+  };
+  //console.log(req);
+  const res = await fetchCompletions(req);
+  //console.log(res);
+  const answer = res.choices[0].message.content;
+  const dt = new DateTime();
+  const data = { dt, prompt, answer };
+  await Deno.writeTextFile(
+    "log/" + dt.day.toString() + ".ndjson",
+    JSON.stringify(data) + "\n"
+  );
+  return answer;
 };
--- test.js
+++
@@ -1,3 +0,0 @@
-import { config } from "https://deno.land/std@0.167.0/dotenv/mod.ts";
-
-console.log(await config());
