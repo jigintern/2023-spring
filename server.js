8dd780921f83bb02a4189ba147493f60720d5076
tai-watanabe <tai-watanabe@jig.jp>
2023-03-23 11:26:54 +0900

    未定義の変数を削除

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ serve(async (req) => {
   console.log(pathname);
 
   if (req.method === "GET" && pathname === "/welcome-message") {
-    return new Response(resp);
+    return new Response("ようこそ");
   }
 
   if (req.method === "POST" && pathname === "/translate-ai") {
