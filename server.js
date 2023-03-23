5e939dbd42e08a6532ec98a43322cc285f5ff520
eiei114 <nowaka37564@gmail.com>
2023-03-23 14:33:49 +0900

    feat ポスト機能レジスター機能、参加者追加機能追加

--- server.js
+++ server.js
@@ -1,42 +1,78 @@
 // deno-lint-ignore-file
-import { fetchChat } from "https://code4fukui.github.io/ai_chat/fetchChat.js";
-import { config } from "https://deno.land/std@0.167.0/dotenv/mod.ts";
-import { serveDir } from "https://deno.land/std@0.180.0/http/file_server.ts";
-import { serve } from "https://deno.land/std@0.180.0/http/server.ts";
+import {fetchChat} from "https://code4fukui.github.io/ai_chat/fetchChat.js";
+import {config} from "https://deno.land/std@0.167.0/dotenv/mod.ts";
+import {serveDir} from "https://deno.land/std@0.180.0/http/file_server.ts";
+import {serve} from "https://deno.land/std@0.180.0/http/server.ts";
+import {v4 as uuidv4} from "https://deno.land/std/uuid/mod.ts";
+
+const posts = [];
 
 serve(async (req) => {
-  console.log(await config());
+    console.log(await config());
 
-  const pathname = new URL(req.url).pathname;
-  console.log(pathname);
+    const pathname = new URL(req.url).pathname;
+    console.log(pathname);
 
-  if (req.method === "GET" && pathname === "/welcome-message") {
-    return new Response("ようこそ");
-  }
+    if (req.method === "GET" && pathname === "/welcome-message") {
+        return new Response("ようこそ");
+    }
 
-  if (req.method === "POST" && pathname === "/translate-ai") {
-    const requestJson = await req.json();
-    const overview = requestJson.overview;
-    const keyword = requestJson.keyword;
-    let resp = null;
-    try {
-      resp = await fetchChat(
-        `${keyword}というキーワードがあてはまる人を集めた${overview}を開こうとしています。
+    if (req.method === "POST" && pathname === "/translate-ai") {
+        const requestJson = await req.json();
+        const overview = requestJson.overview;
+        const keyword = requestJson.keyword;
+        let resp = null;
+        try {
+            resp = await fetchChat(
+                `${keyword}というキーワードがあてはまる人を集めた${overview}を開こうとしています。
         この${overview}の紹介文を200字程度で教えてください。ただし、キーワードの説明を中心に、その他の説明はできるだけ省くようにしてください。`
-      );
-    } catch (error) {
-      console.error("Error while processing chat request:", error);
-      return new Response("Error: " + error.message, { status: 500 });
+            );
+        } catch (error) {
+            console.error("Error while processing chat request:", error);
+            return new Response("Error: " + error.message, {status: 500});
+        }
+        if (resp !== null) {
+            return new Response(resp);
+        }
     }
-    if (resp !== null) {
-      return new Response(resp);
-    }
-  }
 
-  return serveDir(req, {
-    fsRoot: "public",
-    urlRoot: "",
-    showDirListing: true,
-    enableCors: true,
-  });
+    if (req.method === "GET" && pathname === "/get-posts") {
+        // そうじゃない場合は全件返す
+        return new Response(posts);
+    }
+
+    if (req.method === "POST" && pathname === "/register-post") {
+        // リクエストボディを取得する
+        const requestJson = await req.json();
+        //ランダムなIDを生成する
+        const id = uuidv4();
+        const title = requestJson.title;
+        const data = requestJson.data;
+        const name = requestJson.name;
+        const description = requestJson.discrption;
+        //デフォルトは0
+        const participants = 0;
+        // リクエストボディをpostsに追加する
+        posts.push({id, title, data, name, description, participants});
+        // 更新されたpostsを返す
+        return new Response(posts);
+    }
+
+    if (req.method === "POST" && pathname === "/add-participants") {
+        const u = new URL(req.url);
+        const id = u.searchParams.get("id");
+        //postsの中からidが一致するものを探す
+        const post = posts.find((post) => post.id === id);
+        //postの持つparticipantsを1増やす
+        post.participants += 1;
+        //更新されたpostsを返す
+        return new Response(posts);
+    }
+
+    return serveDir(req, {
+        fsRoot: "public",
+        urlRoot: "",
+        showDirListing: true,
+        enableCors: true,
+    });
 });
