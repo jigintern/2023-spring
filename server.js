26fe3626af47c69e718d8e4a150a90abeae3c6b6
tai-watanabe <tai-watanabe@jig.jp>
2023-03-23 10:51:55 +0900

    APIテスト用のページを追加した

--- server.js
+++ server.js
@@ -10,6 +10,36 @@ serve(async (req) => {
     return new Response("jigインターン初コミット！！");
   }
 
+  // GET
+  // クライアント側で単純にデータベースなどに入っている情報を取得したいときに使う
+  if (req.method === "GET" && pathname === "/test-get") {
+    return new Response("test");
+  }
+
+  // GETでもデータを受け取ることはできる
+  if (req.method === "GET" && pathname === "/test-get-json") {
+    const u = new URL(req.url);
+    const params = u.searchParams;
+    const testWord = params.get("testword");
+    if (!testWord) {
+      return new Response("testWordを指定してください");
+    }
+    return new Response(`${testWord}を受け取りました`);
+  }
+
+  // POST
+  // クライアント側のフォームの入力情報等を受け取って保存したりするとかはこっち
+  // 例では、クライアント側から Json で testWord という String 型のデータを受け取ってる想定です
+  if (req.method === "POST" && pathname === "/test-post") {
+    const requestJson = await req.json();
+    const testWord = requestJson.testWord;
+    console.log(testWord);
+    if (!testWord) {
+      return new Response("testWordを指定してください");
+    }
+    return new Response(`${testWord}を受け取りました`);
+  }
+
   return serveDir(req, {
     fsRoot: "public",
     urlRoot: "",
